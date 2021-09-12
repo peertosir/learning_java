bdcdba5d3da922abc80dbe497805a26b6f9d98a5
ntyazhelkov <ntyazhelkov@yandex-team.ru>
2021-09-12 21:00:27 +0300

    fix

--- src/main/java/dev/peertosir/SpamFiltre.java
+++ src/main/java/dev/peertosir/SpamFiltre.java
@@ -4,30 +4,28 @@ import java.util.*;
 
 public class SpamFiltre {
     public static void main(String[] args) throws Exception {
-        Scanner scr1 = new Scanner(System.in);
-        Scanner scr2 = new Scanner(System.in);
+        Scanner scr = new Scanner(System.in);
         String str = "";
         boolean exit = true;
-        List<String> blackList = new ArrayList();
-        blackList.add("XXX");
-        blackList.add("Viagra");
-        blackList.add("MONEYFREE");
-        blackList.add("CASINO");
-        blackList.add("Porn");
+        HashSet<String> blackList = new HashSet<String>();
+        blackList.add("xxx");
+        blackList.add("viagra");
+        blackList.add("moneyfree");
+        blackList.add("casino");
+        blackList.add("porn");
             do {
                 System.out.println("Введите сообщение:");
-                str = scr1.nextLine();
+                str = scr.nextLine();
                 String[] words = str.split("\\W+");
                 for (String word : words) {
-                    System.out.println(word);
                     for (String s : blackList) {
-                        if (word.toLowerCase(Locale.ROOT).equals(s.toLowerCase(Locale.ROOT))) {
+                        if (word.toLowerCase(Locale.ROOT).equals(s)) {
                             System.out.println(word + " - слово из черного списка!");
                         }
                     }
                 }
                 System.out.println("Продолжить ввод? y/n");
-                str = scr2.nextLine();
+                str = scr.nextLine();
                 if (str.equals("y")) {
                     exit = true;
                 } else if (str.equals("n")) {
